e60264cb3e7e29806d736a0a8d9a9aa701e9c446
JuaniOlguin <juani.olguin@gmail.com>
2023-06-28 12:55:03 -0300

    feature: search items endpoint

---
+++ challenge-back-meli/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+// middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// router 
+const router = require('./routes/index');
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
---
+++ challenge-back-meli/routes/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/api/items', (req, res) => {
+    const query = req.query.q
+    const limit = req.query.limit
+    axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
+    .then(function (response) {
+      const items = response.data.results.splice(0, limit);
+      const data = {
+        author: {
+          name: '',
+          lastname: ''
+        },
+        categories: [
+          ...response.data.filters.find(filter => filter.id == 'category').values.map(category => category.name)
+        ],
+        items: items.map(item => {
+          return {
+            id: item.id,
+            title: item.title,
+            price: {
+              currency: item.currency_id,
+              amount: Math.floor(item.price),
+              decimals: Math.floor(item.price) - item.price, 
+            },
+            picture: item.thumbnail,
+            condition: item.condition,
+            free_shipping: item.shipping.free_shipping
+          }
+        })
+      }
+      res.send(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.send(error)
+    })
+    .finally(function () {
+      // always executed
+    });
+});
+
+
+
+module.exports = router;
